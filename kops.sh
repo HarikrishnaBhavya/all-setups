12c5971135bae95cced18777c7b15708f594d989
HarikrishnaBhavya <166035365+HarikrishnaBhavya@users.noreply.github.com>
2024-05-06 17:00:14 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -16,3 +16,13 @@ aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local 
 export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
 kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
 kops update cluster --name rahams.k8s.local --yes --admin
+
+
+
+
+
+#Suggestions:
+ # list clusters with: kops get cluster
+ # edit this cluster with: kops edit cluster bhavya.k8s.local
+ # edit your node instance group: kops edit ig --name=bhavya.k8s.local nodes-us-east-1a
+ # edit your master instance group: kops edit ig --name=bhavya.k8s.local master-us-east-1a
